89783bb8935565587650367ed13a8e0c51f4263e
Holger Mayer <holgermayer@me.com>
2020-06-14 15:22:30 +0200

    Introduced index.html

--- src/main/java/com/example/minidemo/MinidemoApplication.java
+++ src/main/java/com/example/minidemo/MinidemoApplication.java
@@ -18,5 +18,7 @@ public class MinidemoApplication {
        public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
          return String.format("Hello %s!", name);
        }
+       
+ 
 
 }
